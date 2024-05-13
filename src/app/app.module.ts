82127efba6482cccdbd37c392f2813e504932788
Gabriel Prado <gabriel.prado@everis.com>
2024-05-13 17:22:43 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from "@angular/common";
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BusinessService } from './shared/service/business.service';
+import { BaseChartDirective, provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { SharedModule } from './shared/shared.module';
+import { AngularMaterialModule } from './shared/angular-material.module';
+import { AccountsPayableComponent } from './pages/accounts-payable/accounts-payable.component';
+import { AccountsReceivableComponent } from './pages/accounts-receivable/accounts-receivable.component';
+import { PeopleComponent } from './pages/people/people.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+
+registerLocaleData(localePt);
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    AccountsPayableComponent,
+    AccountsReceivableComponent,
+    PeopleComponent,
+    ReportsComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BaseChartDirective,
+    SharedModule,
+  ],
+  providers: [
+    BusinessService,
+    provideCharts(withDefaultRegisterables()),
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ src/app/pages/accounts-payable/accounts-payable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-accounts-payable',
+  templateUrl: './accounts-payable.component.html',
+  styleUrl: './accounts-payable.component.css'
+})
+export class AccountsPayableComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
